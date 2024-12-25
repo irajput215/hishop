340b7da3f5f1b6d92e345a740a08403ca96d419c
irajput215 <eshurajput007@gmail.com>
2024-12-25 15:06:50 +1100

    checkpoint : { models, seeding data, middleware, controller}

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
+dotenv.config();
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { dot } from 'node:test/reporters';
+
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
+
 
 const port = process.env.PORT || 5002;
 
@@ -14,13 +19,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
